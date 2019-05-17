b9375bc2208c0d857f00b9f082384b0f1ff9f7df
Mehdi Abaakouk <sileht@sileht.net>
2019-05-17 08:57:36 -0400

    Initial commit

---
+++ gen.py
@@ -0,0 +1,38 @@
+
+import os
+import sys
+import requests
+
+
+def get_repos():
+    url = "https://api.github.com/orgs/wazo-pbx/repos"
+    session = requests.Session()
+    while True:
+        response = session.get(url, headers={
+            "Accept": "application/vnd.github.machine-man-preview+json",
+            "User-Agent": "PyGithub/Python",
+        }, auth=("sileht", os.getenv("GITHUB_TOKEN"))
+        if response.ok:
+            for r in response.json():
+                yield r
+            if "next" in response.links:
+                url = response.links["next"]["url"]
+                continue
+            else:
+                return
+        else:
+            raise Exception(response.text)
+
+with open("default.xml", 'w') as f:
+    f.write('''<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+
+    <remote name="origin" fetch="https://github.com" />
+    <default revision="refs/heads/master" remote="origin" sync-j="4" />
+
+''')
+    for r in get_repos():
+        if not r["archived"] and not r['disabled']:
+            f.write('    <project name="%(full_name)s" path="%(name)s"/>\n' % r)
+
+    f.write('</manifest>\n')
